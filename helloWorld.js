ce9aadbf4946cac0c3f3f2de52a1753b91c9ebc2
hagar-gamal <hagar28gamal@gmail.com>
2025-05-18 12:34:04 +0300

    hello world done

---
+++ helloWorld.js
@@ -0,0 +1,16 @@
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  printData() {
+    return "Name is " + this.name + " , and age is " + this.age;
+  }
+  printHelloWorld() {
+    console.log("Hello World");
+  }
+
+}
+const ME = new Person("HAGAR", 22);
+console.log(ME.printData());
+ME.printHelloWorld()
