0d5fcd4529fcca9d41c63cacbe2132ca1ee934e4
hagar-gamal <hagar28gamal@gmail.com>
2025-05-18 12:45:13 +0300

    hello git

--- helloITI.js
+++ helloITI.js
@@ -1,16 +1,20 @@
 class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-  printData() {
-    return "Name is " + this.name + " , and age is " + this.age;
-  }
-  printHelloITI() {
-    console.log("Hello ITI");
-  }
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    printData() {
+        return "Name is " + this.name + " , and age is " + this.age;
+    }
+    printHelloITI() {
+        console.log("Hello ITI");
+    }
+    printHelloGit() {
+        console.log("Hello Git");
+    }
 
 }
 const ME = new Person("HAGAR", 22);
 console.log(ME.printData());
+ME.printHelloITI()
 ME.printHelloITI()
